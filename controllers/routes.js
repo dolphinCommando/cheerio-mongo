133bdefeb10bd3531827981fa3bab12c5381e165
Matthew Buchanan <matt@Matthews-MacBook-Air.local>
2018-07-03 13:15:08 -0400

    put and delete routes for comments

--- controllers/routes.js
+++ controllers/routes.js
@@ -21,9 +21,8 @@ const startApp = function(app) {
   app.post('/notes', function(req, res) {
     db.Article.find({_id: mongoose.Types.ObjectId(req.body.id)}).then((dbArticle) => {
       console.log('Article found');
-      console.log(dbArticle);
       if (dbArticle[0].note !== null && dbArticle[0].note !== undefined) {
-        console.log('note exists')
+        //console.log(dbArticle);
         //var noteIds = dbArticle.note.map(ref => {mongoose.Types.ObjectId(ref)});
         db.Note.find({_id: dbArticle[0].note}).then((dbData) => {
           console.log('Note found');
@@ -32,7 +31,8 @@ const startApp = function(app) {
         });        
       }
       else {
-        res.send({title: 'No Comments', body:'Nothing here'});
+        console.log('no notes');
+        res.sendStatus(404);
       } 
     }).catch(err => {
       console.log(err);
@@ -40,18 +40,69 @@ const startApp = function(app) {
     });
   });
 
-  app.post('/notes/submit', function(req, res) {
-    var newNote = req.body.comment;
-    var id = req.body.id;
+  app.post('/notes/:id', function(req, res) {
+    var newNote = req.body;
+    var id = req.params.id;
     db.Note.create(newNote).then((dbNote) => {
+      db.Article
+      .findOneAndUpdate({_id: mongoose.Types.ObjectId(id)}, {$push: {note: dbNote._id}}, {new: true})
+      .then((dbArticle) => {
         res.send(dbNote);
-        return db.Article.findOneAndUpdate({_id: mongoose.Types.ObjectId(id)}, {$push: {note: dbNote._id}}, {new: true});
-      }).then((dbArticle) => {
         console.log(dbArticle);
       })
       .catch((err) => {
         console.log(err);
         res.sendStatus(500);
+      });
+    });
+  });
+
+  app.put('/notes/:id/:noteid', function(req, res) {
+    var newNote = req.body;
+    console.log('newNote ' + newNote);
+    var noteId = req.params.noteid;
+    var articleId = req.params.id;
+    db.Note
+      .update({_id: mongoose.Types.ObjectId(noteId)}, {$set: {title: newNote.title, body: newNote.body}})
+      .then((dbNote) => {
+        db.Article
+          .find({_id: mongoose.Types.ObjectId(articleId)})
+          .populate('note')
+          .then((dbArticle) => {
+            res.send(dbNote);
+            console.log(dbArticle);
+          })
+          .catch(err => {
+            console.log(err);
+            res.sendStatus(500)
+          });
+      }) 
+      .catch(err => {
+        res.sendStatus(404);
+      }) 
+  });
+
+  app.delete('/notes/:id/:noteid', function(req, res) {
+    var noteId = req.params.noteid;
+    var articleId = req.params.id;
+    db.Note
+      .deleteOne({_id: mongoose.Types.ObjectId(noteId)})
+      .then((dbNote) => {
+        db.Article
+          .find({_id: mongoose.Types.ObjectId(articleId)})
+          .populate('note')
+          .then((dbArticle) => {
+            console.log(dbArticle);
+            res.sendStatus(200);
+          })
+          .catch(err => {
+            console.log(err);
+            res.sendStatus(500)
+          }); 
+        })    
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(404);
       });
   });
 }
--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 
   $('.notes').click(function(event) {
     event.preventDefault();
-    $('.modalComments').empty();
     var articleId = $(this).attr('data-id');
     $('#modalNotes').attr('data-articleId', articleId);
     getNotes(articleId);
@@ -10,30 +9,51 @@ $(document).ready(function() {
 
   $('#compose').click(function(event) {
     event.preventDefault();
-    var id = $('#modalNotes').attr('data-articleId');
-    $('#modalNotes').modal({show: false});
-    $.post('/notes/submit', {
-      comment: {
-        title: $('#commentTitle').val().trim(),
-        body: $('#commentBody').val().trim()
-      },
-      id: id
-    }, function(data) {
+    var articleId = $('#modalNotes').attr('data-articleId');
+    var noteId = $('#modalCompose').attr('data-noteid');
+    var role = $('#modalCompose').attr('data-role');
+    var comment = {
+      title: $('#commentTitle').val().trim(),
+      body: $('#commentBody').val().trim()
+    };
+    postOrUpdate(articleId, noteId, role, comment, function(data) {
       console.log(data);
-      $('#commentTitle').val('');
-      $('#commentBody').val('');
-      console.log('data id ' + id)
-      getNotes(id);
+      getNotes(articleId);
+    })
+    
+  });
 
+  $('body').on('click', '.commentEdit', function(event) {
+    event.preventDefault();
+    var commentParent = $(this).parent();
+    console.log(commentParent);
+    $('#commentTitle').val(commentParent.children('h6').text());
+    $('#commentBody').val(commentParent.children('p').text());
+    $('#modalCompose').attr('data-role', 'put');
+    $('#modalCompose').attr('data-noteid', commentParent.attr('data-id'));
+  });
+
+  $('body').on('click', '.commentDelete', function(event) {
+    event.preventDefault();
+    var commentParent = $(this).parent();
+    var noteId = commentParent.attr('data-id');
+    var articleId = $('#modalNotes').attr('data-articleId');
+    $.ajax({
+      method: "DELETE",
+      url: `/notes/${articleId}/${noteId}`
+    }, function(data) {
+      console.log('Note deleted');
+      getNotes(articleId);
     });
   });
 
   function getNotes(articleId) {
+    $('.modalComments').empty();
     $.post(`/notes`, {id: articleId}, function(data) {
       console.log(data);
       data.forEach(function(item) {
         $('.modalComments').prepend(`
-        <div>
+        <div data-id="${item._id}">
         <h6>${item.title}</h6>
         <p>${item.body}</p>
         <button class="btn btn-warning commentEdit">Edit</button>
@@ -42,7 +62,31 @@ $(document).ready(function() {
         `);
       });
     });
+    $('#commentTitle').val('');
+    $('#commentBody').val('');
+    $('#modalCompose').attr('data-role', 'post');
+    $('#modalCompose').attr('data-noteid', '');
     $('#modalNotes').modal({show: true});
+  };
+
+  function postOrUpdate(articleId, noteId, role, comment, callback) {
+    if (role === 'post') {
+      $.post(`/notes/${articleId}`, comment, function(data) {
+        callback(data);
+      });
+    }
+    else if (role === 'put') {
+      $.ajax({
+        method: "PUT",
+        url: `/notes/${articleId}/${noteId}`, 
+        data: comment
+      }, function(data) {
+        callback(data);
+      });
+    }
+    else {
+      console.log('Error in #modalCompose data-role');
+    }
   }
 
 });
